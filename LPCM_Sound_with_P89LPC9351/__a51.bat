e61a576a7d7132f7d0106dc8f70b06f5c1ca8315
Zihao Pu <puzihao2018@outlook.com>
2020-02-01 22:01:31 -0800

    added mov32 to math32

---
+++ LPCM_Sound_with_P89LPC9351/__a51.bat
@@ -0,0 +1,5 @@
+@echo off
+Z:
+cd "\Desktop\ELEC 291\Project 1\Project1_Code\LPCM_Sound_with_P89LPC9351\"
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "Z:\Desktop\ELEC 291\Project 1\Project1_Code\LPCM_Sound_with_P89LPC9351\LPC9351_Receiver.asm"
+echo Crosside_Action Set_Hex_File Z:\Desktop\ELEC 291\Project 1\Project1_Code\LPCM_Sound_with_P89LPC9351\LPC9351_Receiver.HEX
