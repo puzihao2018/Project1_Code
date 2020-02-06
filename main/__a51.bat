1f0d9a3f6096a8b29eeb3bd66dac6a9fc2e7d86c
sdkadu <ziqiang.chen8@gmail.com>
2020-02-06 14:41:55 -0800

    c

--- main/__a51.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-Z:
-cd "\Desktop\ELEC 291\Project 1\Project1_Code\main\"
-"C:\CrossIDE\Call51\Bin\a51.exe" -l "Z:\Desktop\ELEC 291\Project 1\Project1_Code\main\temp_test.asm"
-echo Crosside_Action Set_Hex_File Z:\Desktop\ELEC 291\Project 1\Project1_Code\main\temp_test.HEX
